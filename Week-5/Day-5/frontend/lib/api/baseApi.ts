85744b26848fd339009b042daa5c510613f5af0c
Fahad Shahbaz Intern <muhammad_fahad_intern@netixsol.com>
2025-09-03 12:35:07 +0500

    chore: update backend link

--- Week-5/Day-5/frontend/lib/api/baseApi.ts
+++ Week-5/Day-5/frontend/lib/api/baseApi.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import type { RootState } from "../store";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000",
+  baseUrl: process.env.NEXT_PUBLIC_API_URL || "https://car-auction-backend-nine.vercel.app/",
   prepareHeaders: (headers, { getState }) => {
     const token = (getState() as RootState).auth.token;
 
